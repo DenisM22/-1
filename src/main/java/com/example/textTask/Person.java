6d709c85de056b4c8b73bb284d05e4d625e591cf
Denis <denis.mezenev@yandex.ru>
2024-06-25 19:56:49 +0300

    Описание через стереотипные аннотации

---
+++ src/main/java/com/example/textTask/Person.java
@@ -0,0 +1,30 @@
+package com.example.textTask;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Person {
+
+    private String name;
+    public List<Animal> animals;
+
+    @Autowired
+    public Person(List<Animal> animals) {
+        this.animals = new ArrayList<>(animals);
+    }
+
+    public void addAnimal(Animal animal) {
+        this.animals.add(animal);
+    }
+
+    public void showAnimals() {
+        for (Animal animal : this.animals) {
+            System.out.println(animal.getName());
+        }
+    }
+
+}
